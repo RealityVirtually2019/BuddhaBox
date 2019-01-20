aa9e189a6172b8aa044127963a1201ce46b0e548
cvachha <cyrus.vachha@yahoo.com>
2019-01-19 20:31:27 -0800

    Timer for last scene
    timer last scene for the end

--- Assets/BuddhaBox/Scripts/GameStates/StateIntensity3.cs
+++ Assets/BuddhaBox/Scripts/GameStates/StateIntensity3.cs
@@ -7,15 +7,34 @@ public class StateIntensity3 : StateIntensityBase
     //activate rain
     public GameObject[] softClouds;
     public GameObject[] hardClouds;
-
+    bool isFocused;
+    float counter;
+    void Start()
+    {
+        isFocused = false;
+        counter = 0;
+    }
+    void Update()
+    {
+        if (isFocused)
+        {
+           
+            counter += Time.deltaTime;
+           
+        }
+        if (counter > 4.0f)
+        {
+            Debug.Log("It has been 4 seconds");
+        }
+    }
 
     public override void GainFocus()
     {
         //play softCloud particles
         for (int i = 0; i < 6; i++)
         {
+            isFocused = true;
             hardClouds[i].SetActive(false);
-
             softClouds[i].SetActive(true);
         }
         base.GainFocus();
@@ -25,6 +44,8 @@ public class StateIntensity3 : StateIntensityBase
     public override void LoseFocus()
     {
         //disable softCloud particles
+        counter = 0;
+        isFocused = false;
         for (int i = 0; i < 6; i++)
         {
             hardClouds[i].SetActive(true);
