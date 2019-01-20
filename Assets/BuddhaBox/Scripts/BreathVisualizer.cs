a343ceb80336609c83591469791c489eda07a759
Ziba Scott <ziba@popcannibal.com>
2019-01-20 10:47:45 -0500

    breath work. narrator pacing.

---
+++ Assets/BuddhaBox/Scripts/BreathVisualizer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathVisualizer : ModuleBase
+{
+    public ParticleSystem particles;
+    public ParticleSystem.EmissionModule emission;
+
+    public BreathDetector detector;
+
+    public TMPro.TextMeshProUGUI text;
+
+    public float particleEmissionWhenBreathing = 50;
+
+    public void Start()
+    {
+        detector = GameManager.instance.modules.Get<BreathDetector>();
+        emission = particles.emission;
+        StartCoroutine(DelayedParticleDeactivate());
+    }
+
+    IEnumerator DelayedParticleDeactivate()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(4);
+            if(detector.breathState != BreathDetector.BREATH_STATE.BREATHING){
+                particles.gameObject.SetActive(false);
+                yield return new WaitForEndOfFrame();
+                particles.gameObject.SetActive(true);
+            }
+         
+
+
+        }
+
+
+    }
+
+    public override void DoUpdate()
+    {
+        switch (detector.breathState)
+        {
+            case BreathDetector.BREATH_STATE.BREATHING:
+                emission.rateOverTime = particleEmissionWhenBreathing;
+                text.text = "Breathing: "+detector.DbValue;
+                particles.gameObject.SetActive(true);
+
+                break;
+            case BreathDetector.BREATH_STATE.NOT_BREATHING:
+                emission.rateOverTime = 0;
+                text.text = "Not breathing: " + detector.DbValue;
+                break;
+        }
+    }
+}
